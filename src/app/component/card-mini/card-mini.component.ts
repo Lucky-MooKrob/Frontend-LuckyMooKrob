c73ab62842d2eb3de3dad53d7da5d34560c9cb86
Kantarat Krutta <aomkktr@Szols-MacBook-Pro.local>
2022-08-23 00:38:01 +0700

    refactor card mini

--- src/app/component/card-mini/card-mini.component.ts
+++ src/app/component/card-mini/card-mini.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Input } from '@angular/core';
+import { InsuranceProduct } from 'src/app/model/product';
 @Component({
   selector: 'app-card-mini',
   templateUrl: './card-mini.component.html',
   styleUrls: ['./card-mini.component.scss']
 })
 export class CardMiniComponent implements OnInit {
+
+  @Input() item :InsuranceProduct = {
+    insuranceMiniDetailsID: 0,
+    insuranceMiniDetailsName: '',
+    insuranceMiniDetailsPic: '',
+    insuranceMiniDetailsPrice: 0,
+    insuranceMiniDetailsFBulletin: '',
+    insuranceMiniDetailsSBulletin: '',
+    insuranceMiniDetailsTBulletin: '',
+    insuranceMiniDetailsStartAge: 0,
+    insuranceMiniDetailsEndAge: 0,
+    insuranceMiniDetailsDisease: ''
+  };
 
   constructor() { }
 
